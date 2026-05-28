fa3a1787631227c9e0bc3d4a1231772bb5de1ff8
Thái Văn Lâm <thaivanlam373@gmail.com>
2026-05-28 12:26:41 +0700

    update

--- notification-service/src/main/java/vn/vti/dtn2504/notificationservice/config/RabbitMQConfig.java
+++ notification-service/src/main/java/vn/vti/dtn2504/notificationservice/config/RabbitMQConfig.java
@@ -6,9 +6,22 @@ import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class RabbitMQConfig {
+    @Value("${queue.notification.queue}")
+    private String notificationQueue;
+
+    @Value("${queue.notification.exchange}")
+    private String notificationExchange;
+
+    @Value("${queue.notification.routing-key}")
+    private String notificationRoutingKey;
+
     @Bean
     public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
             ConnectionFactory connectionFactory) {
@@ -21,7 +34,19 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    public Queue notificationQueue() {
-        return new Queue("notification-queue", true);
+    public Queue notificationQueueBean() {
+        return new Queue(notificationQueue, true);
+    }
+
+    @Bean
+    public DirectExchange notificationExchange() {
+        return new DirectExchange(notificationExchange, true, false);
+    }
+
+    @Bean
+    public Binding notificationBinding() {
+        return BindingBuilder.bind(notificationQueueBean())
+                .to(notificationExchange())
+                .with(notificationRoutingKey);
     }
 }
